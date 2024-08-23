a26f88ee34c3de33ac77252b4f716258876155c5
HUYNH THANH NHAN <huynhthanhnhan632004@gmail.com>
2024-08-23 08:46:03 +0700

    update nhan update code

--- interfaces/common/user.interface.ts
+++ interfaces/common/user.interface.ts
@@ -1,15 +1,2 @@
-
-export interface IUser {
-    _id: any
-    user_name: string;
-    user_email: string;
-    user_password: string;
-    user_phone: number;
-    user_address: string;
-    user_image: string;
-    is_active: boolean;
-    codeId?: string;
-    timeExpired?: string; // Thời gian hết hạn xác nhận Email
-    user_role?: "USER" | "ADMIN";
-    user_accountType?: "ACTIVE" | "BLOCK";
-}
+import { UserInterfaceCommon } from "library-common"
+export interface IUser extends UserInterfaceCommon.IUser { }
